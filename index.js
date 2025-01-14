df45b17c9e62593caaa6bcb3babc5d6376b075bf
rambleboy <168521202+rambleboy@users.noreply.github.com>
2025-01-14 16:05:43 +0700

    test cors *

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ const logQuery = (query, replacements) => {
 
 // cors
 app.use(cors({
-    origin: ['http://localhost:3000','http://localhost:4173', 'https://lawstd.rmu.ac.th'],
+    origin: ['*'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // กำหนดวิธีการที่อนุญาต
     credentials: true // อนุญาตให้ส่ง Cookies
 }));
 
-console.log("xxxxxxxxxx- cors origin = http://localhost:3000','http://localhost:4173', 'https://lawstd.rmu.ac.th'");
+console.log("xxxxxxxxxx- cors origin = *'");
 
 // API สำหรับล็อกอินผู้ดูแลระบบ
 app.post('/api/login-admin', async (req, res, next) => {
