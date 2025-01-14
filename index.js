49514873d39736670df7560c7e5938bde3d1a1e8
rambleboy <168521202+rambleboy@users.noreply.github.com>
2025-01-14 14:08:16 +0700

    use bcryptjs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db'); // เชื่อมต่อฐานข้อมูล
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 const port = 3000;
