c342bc77d2cb5b8ce259d1f8b162d1a5e0ac97ee
rambledev <techo.dev2024@gmail.com>
2025-02-28 15:11:31 +0700

    Update subjectRoutes.js

--- routes/subjectRoutes.js
+++ routes/subjectRoutes.js
@@ -4,6 +4,8 @@ const subjectController = require('../controllers/subjectController');
 const router = express.Router();
 
 router.get('/', subjectController.getAllSubjects);
+
+router.get('/subjectData', subjectController.getDataSubjects);
 router.get('/:sub_code', subjectController.getSubjectByCode);
 router.post('/', subjectController.createSubject); // เพิ่ม route สำหรับ POST
 router.delete('/:id', subjectController.deleteSubject);
